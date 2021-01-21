9729b6d9ca75fe52cad8de60277d8e4034af5547
Ashley Prosser <ashprosser@live.co.uk>
2021-01-21 21:57:27 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -14,3 +14,37 @@ variable "secondary_locations" {
   type        = list(string)
   default     = []
 }
+
+variable "custom_domain_certificate" {
+  description = "HTTPS Certificate for APIM Custom Domain"
+  type = object({
+    certificate_name = string
+    key_vault_name = string
+    key_vault_resource_group_name = string
+  })
+}
+
+variable "portal_custom_domain_certificate" {
+  description = "HTTPS Certificate for APIM Portal Custom Domain"
+  type = object({
+    certificate_name = string
+    key_vault_name = string
+    key_vault_resource_group_name = string
+  })
+}
+
+variable "management_custom_domain_certificate" {
+  description = "HTTPS Certificate for APIM Management Custom Domain"
+  type = object({
+    certificate_name = string
+    key_vault_name = string
+    key_vault_resource_group_name = string
+  })
+}
+
+variable "dns_zone" {
+  type = object({
+      name = string
+      resource_group_name = string
+  })
+}
